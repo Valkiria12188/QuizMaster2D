366fce8ef623f51b408b9936a49d44d0dbcee7f2
Mateusz Proc <44548853+Valkiria12188@users.noreply.github.com>
2022-02-26 22:35:08 +0100

    Ukończenie 62 lekcji

--- QuizMaster/Assets/Scripts/Quiz.cs
+++ QuizMaster/Assets/Scripts/Quiz.cs
@@ -6,31 +6,60 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
+
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+    bool hasAnsweredEarly;
+
+    [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
+
+    [Header("Timer")]
+    [SerializeField] Image tiemrImage;
+    Timer timer;
+
 
 
     void Start()
     {
-        questionText.text = question.GetQuestion();
-        
-        for( int i=0;i<answerButtons.Length;i++)
+        timer = FindObjectOfType<Timer>();
+        DisplayQuestion();
+    }
+
+    void Update()
+    {
+        tiemrImage.fillAmount = timer.fillFraction;
+        if(timer.loadNextQuestion)
         {
-           TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-           buttonText.text = question.GetAnswer(i);
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
         }
-       
+        else if(!hasAnsweredEarly && !timer.isAnsweringQuestion )
+        {
+            DisplayAnswer(-1);// -1 jest wyjsciem wynikajacym z sytuacji w któej potrzeba indeksu ktróego tu  nie podam wiêæ podaje uniwersalny który nie wp³ynie na dzia³anie
+            SetButtonsState(false);
+        }
     }
 
     public void OnAnswerSelected(int index)
     {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
+        SetButtonsState(false);
+        timer.CancelTimer();
+    }
+
+    void DisplayAnswer(int index)
+    {
         Image buttonImage;
 
-        if (index==question.GetCorrectAnswerIndex())
+        if (index == question.GetCorrectAnswerIndex())
         {
             questionText.text = "Poprawna OdpowiedŸ!";
             buttonImage = answerButtons[index].GetComponent<Image>();
@@ -45,5 +74,45 @@ public class Quiz : MonoBehaviour
             buttonImage.sprite = correctAnswerSprite;
         }
     }
-    
+
+    void GetNextQuestion()
+    {
+        SetButtonsState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+
+    }
+
+    void SetButtonsState(bool state)
+    {
+        for(int i=0; i<answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        Image buttonImage;
+        for (int i = 0; i < answerButtons.Length; i++)
+        {       
+            buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
+    }
+
+
 }
